00fcc3491abaf5bfac85bf95700fd3c0337be0f9
Matheus Santos <matheusmendes1@hotmail.com>
2020-04-23 23:45:12 -0300

    Folder migration

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import 'reflect-metadata';
+import cors from 'cors';
 
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
@@ -11,6 +12,7 @@ import './database';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
