28b706c666b557c878cb418efa0a46f9a21bc8bc
Matheus Santos <matheusmendes1@hotmail.com>
2020-05-06 00:00:23 -0300

    Coverage report

---
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,21 @@
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '12131321',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('12131321');
+  });
+
+  // it('should not be able to create two appointments on the same time', () => {});
+});
