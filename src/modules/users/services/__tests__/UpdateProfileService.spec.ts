8769094d6a2d4447f9933ed86bb0798b564a4817
Matheus Santos <matheusmendes1@hotmail.com>
2020-05-14 11:47:56 -0300

    Rota e controller de perfil

--- src/modules/users/services/__tests__/UpdateProfileService.spec.ts
+++ src/modules/users/services/__tests__/UpdateProfileService.spec.ts
@@ -57,6 +57,16 @@ describe('UpdateProfile', () => {
     ).rejects.toBeInstanceOf(AppError);
   });
 
+  it('should not be able to update the profile from non-existing user', async () => {
+    await expect(
+      updateProfile.execute({
+        user_id: 'non-existing user',
+        name: 'Test',
+        email: 'test@example.com',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
   it('should be able to update the password', async () => {
     const user = await fakeUsersRepository.create({
       name: 'John Doe',
